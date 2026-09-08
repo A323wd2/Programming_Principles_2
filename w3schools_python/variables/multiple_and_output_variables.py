86cb09e2f7a65107ab34c8f0d18c13e5b7dc5b9a
A323wd2 <bueshevaaiym@gmail.com>
2026-09-08 15:44:09 +0500

    Create multiple_and_output_variables.py

---
+++ w3schools_python/variables/multiple_and_output_variables.py
@@ -0,0 +1,34 @@
+#many values to multiple variables in one line
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#one value to multiple variables in one line
+a = b = c = "Orange"
+print(a)
+print(b)
+print(c)
+
+#unpacking
+fruits = ["apple", "banana", "cherry"]
+l, m, n = fruits
+print(l)
+print(m)
+print(n)
+
+#output with print
+q = "Python is awesome"
+print(q)
+
+#outputing multiple variables separated by comma in print()
+d = "Python"
+e = "is"
+f = "awesome"
+print(d, e, f)
+
+#using + operator to output multiple variables
+g = "Python "
+h = "is "
+i = "awesome"
+print(g + h + i)
