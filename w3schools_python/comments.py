a385e9e82ffd19623e098fdb3825c19189dc72eb
A323wd2 <bueshevaaiym@gmail.com>
2026-09-08 15:18:20 +0500

    Create comments.py

---
+++ w3schools_python/comments.py
@@ -0,0 +1,19 @@
+#this is a comment.
+print("this is a visible code line")
+
+print("this is a visible code line too") #this is a comment too
+
+#print("this is code that is written as a comment")
+print("this is an actual code")
+
+#this is a comment
+#written in
+#more than just one line
+print("this is a visible code line")
+
+"""
+This is a comment
+written in
+more than just one line too (it's a string not assigned to a variable so it's ignored by python)
+"""
+print("this is a visible code line too")
