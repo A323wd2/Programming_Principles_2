de5becf6fef701972baa6bb4ce64f173f52c50a9
A323wd2 <bueshevaaiym@gmail.com>
2026-09-08 15:07:58 +0500

    Create syntax.py

---
+++ w3schools_python/syntax.py
@@ -0,0 +1,20 @@
+#python identation
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+#python variables
+x = 5
+y = "Hello, World!"
+
+#python comments
+#this is a comment.
+
+#statements
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+#statements with semicolon
+print("Hello"); print("How are you?"); print("Bye bye!")
