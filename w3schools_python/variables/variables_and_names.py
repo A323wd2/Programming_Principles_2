989ad3c713e6034f6c06ac90e0f24599799a45a9
A323wd2 <bueshevaaiym@gmail.com>
2026-09-08 15:33:50 +0500

    Create variables_and_names.py

---
+++ w3schools_python/variables/variables_and_names.py
@@ -0,0 +1,45 @@
+#creating variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#changing type and value of a variable after it's been set
+c = 4       # c is of type int
+c = "Sally" # c is now of type str
+print(c)
+
+#casting
+q = str(3)    # q will be '3'
+w = int(3)    # w will be 3
+e = float(3)  # e will be 3.0
+
+#getting type of a variable with type() functiomn
+v = 5
+b = "Boat"
+print(type(v))
+print(type(b))
+
+#string variables can be declared with both single or double quotes
+n = "Name"
+# is the same as
+n = 'Name'
+
+#variable names are case sensitive
+a = 4
+A = "Salty"
+#A will not overwrite a
+
+#legal variable names
+myvar = "John"
+my_var = "Sandy"
+_my_var = "Maria"
+myVar = "Jeorge"
+MYVAR = "Charlie"
+myvar2 = "Eric"
+#camel case
+myVariableName = "Winnie"
+#pascal case
+MyVariableName = "Rick"
+#snake case
+my_variable_name = "Dinn"
