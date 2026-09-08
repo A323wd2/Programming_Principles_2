a8e48dcbcf78d4b54cb5d5eba22e3a303857208b
A323wd2 <bueshevaaiym@gmail.com>
2026-09-08 15:00:05 +0500

    Create getting_started.py
    Add comments and version check in getting_started.py

---
+++ w3schools_python/getting_started.py
@@ -0,0 +1,8 @@
+#File extension for python is py
+#command line syntax for checking if python is installed on your computer is python --version
+#syntax to exit the Python command line interface is exit()
+
+#checking the Python version of the editor:
+import sys
+
+print(sys.version)
