7734980d323594f9dd0331035544a827f9c2eadd
A323wd2 <bueshevaaiym@gmail.com>
2026-09-10 22:04:50 +0500

    Create format_and_escape.py

---
+++ w3schools_python/strings/format_and_escape.py
@@ -0,0 +1,21 @@
+#f-strings
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#placeholder
+price = 59
+txt2 = f"The price is {price} dollars"
+print(txt2)
+
+#with modifier
+price2 = 59
+txt3 = f"The price is {price:.2f} dollars"
+print(txt3)
+
+#with math operation
+txt4 = f"The price is {20 * 59} dollars"
+print(txt4)
+
+#escape character \"
+txt5 = "We are the so-called \"Vikings\" from the north."
