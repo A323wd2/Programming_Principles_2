64889eaf07688b0bc07889ac83e6206436662857
A323wd2 <bueshevaaiym@gmail.com>
2026-09-15 11:19:09 +0500

    Create if.py

---
+++ Practice_2/if_else/if.py
@@ -0,0 +1,17 @@
+#if
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+#multiple statements inside an if block
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+#boolean variable:
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
