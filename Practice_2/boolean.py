c97113fe2df4995544b26685f525dca0dd5d3406
A323wd2 <bueshevaaiym@gmail.com>
2026-09-15 14:11:51 +0500

    Update boolean.py

--- Practice_2/boolean.py
+++ Practice_2/boolean.py
@@ -1,1 +1,24 @@
+#boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
 
+#if true and false
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#evaluating string and int
+print(bool("Hello"))
+print(bool(15))
+
+#evaluating variables
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
